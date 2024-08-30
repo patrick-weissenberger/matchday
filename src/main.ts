4ea0877bb43cdc62597d2f79506a9b795c621a9e
Patrick Weissenberger <pweissenberger@gmx.ch>
2024-08-30 20:37:22 +0200

    removed imports

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from "vue"
 import { createPinia } from "pinia"
 import App from "@/App.vue"
 import router from "@/router"
-import useStoreMatches from "@/stores/useStoreMatches"
 import ContainerOut from "@/components/containers/ContainerOut.vue"
 import ContainerIn from "@/components/containers/ContainerIn.vue"
 import "@/assets/css/main.css"
@@ -19,9 +18,5 @@ app.use(router)
 
 app.component("ContainerOut", ContainerOut)
 app.component("ContainerIn", ContainerIn)
-
-// const storeMatches = useStoreMatches()
-// await storeMatches.fetchMatches()
-
 
 app.mount("#app")
