211278ddad492e89eb08dddf4467cc938018b85c
Patrick Weissenberger <pweissenberger@gmx.ch>
2024-08-30 09:45:33 +0200

    finished favoriteTeam functionality

--- src/stores/useStoreTeams.ts
+++ src/stores/useStoreTeams.ts
@@ -3,7 +3,6 @@ import useStoreMatches from "@/stores/useStoreMatches"
 import { computed } from "vue"
 import type { Match } from "@/databases/supabase/types/supabase.single.types"
 import type { Team } from "@/interfaces/interfaces"
-import sortTeams from "@/utils/sortTeams"
 import { useStorage } from "@vueuse/core"
 
 
@@ -14,18 +13,41 @@ const useStoreTeams = defineStore("useStoreTeams", () => {
 
     const storeMatches = useStoreMatches()
 
-    const teams = computed(() => {
-        let uniqueTeamNames: Set<Team> = new Set()
+    const teams = computed((): Team[] => {
+        let teams: Team[] = []
+        let seenTeams: Set<string> = new Set()
 
         storeMatches.matches.forEach((match: Match) => {
-            uniqueTeamNames.add({ name: match.team_home_name })
-            uniqueTeamNames.add({ name: match.team_away_name })
+            const teamHomeNameLower = match.team_home_name.toLowerCase()
+            const teamAwayNameLower = match.team_away_name.toLowerCase()
+
+            if (!seenTeams.has(teamHomeNameLower)) {
+                teams.push({ name: match.team_home_name })
+                seenTeams.add(teamHomeNameLower)
+            }
+            if (!seenTeams.has(teamAwayNameLower)) {
+                teams.push({ name: match.team_away_name })
+                seenTeams.add(teamAwayNameLower)
+            }
         })
 
-        uniqueTeamNames = sortTeams(uniqueTeamNames)
+        teams.sort((t1: Team, t2: Team) => t1.name.localeCompare(t2.name))
 
-        return uniqueTeamNames
+        return teams
     })
+
+    // const teams = computed((): Team[] => {
+    //     let uniqueTeamNames: Set<Team> = new Set<Team>()
+
+    //     storeMatches.matches.forEach((match: Match) => {
+    //         uniqueTeamNames.add({ name: match.team_home_name.toLowerCase() })
+    //         uniqueTeamNames.add({ name: match.team_away_name.toLowerCase() })
+    //     })
+
+    //     console.log("Teams:", uniqueTeamNames)
+
+    //     return sortTeams(uniqueTeamNames)
+    // })
 
     let favoriteTeam = useStorage("favoriteTeam", "")
 
